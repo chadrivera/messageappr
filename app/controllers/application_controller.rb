9c64111c70e4f608bbb477446cb93f2a31a97aa7
chadrivera <chad.rivera@gmail.com>
2015-02-16 18:16:12 -0500

    Updated time zone methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  around_filter :set_time_zone
+
+  def set_time_zone(&block)
+    Time.use_zone("Eastern Time (US & Canada)",&block)
+  end
 end
